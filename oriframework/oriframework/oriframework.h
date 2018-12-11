fd37cbb3fbab5ee0a90024aca1f46b9fd064afee
Shama Ahlawat <shama@oriserve.com>
2018-12-11 12:00:26 +0530

    Finished framework

---
+++ oriframework/oriframework/oriframework.h
@@ -0,0 +1,19 @@
+//
+//  oriframework.h
+//  oriframework
+//
+//  Created by Shama Ahlawat on 11/12/18.
+//  Copyright © 2018 oriserve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for oriframework.
+FOUNDATION_EXPORT double oriframeworkVersionNumber;
+
+//! Project version string for oriframework.
+FOUNDATION_EXPORT const unsigned char oriframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <oriframework/PublicHeader.h>
+
+
